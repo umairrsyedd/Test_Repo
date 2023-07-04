5cdb1d3d8d43122b594fd24eb4dbbc74290e16de
Syed Umair <syed.umair@furlenco.com>
2023-07-04 12:06:22 +0530

    Added Third Commit for the Feature branch

--- main.go
+++ main.go
@@ -6,4 +6,6 @@ func main() {
 	print("First Commit for the Feature Branch")
 
 	print("Second Commit for the Feature  Branch")
+
+	print("Third Commit for the Feature Branch after which We will merge into the main branch")
 }
