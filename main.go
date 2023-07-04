6ca1ef6ed4b270df1d1bc9692752d1dc53fa4654
Syed Umair <syed.umair@furlenco.com>
2023-07-04 12:06:50 +0530

    Feature/some dev (#1)
    * Added First Commit for the First Branch
    
    * Added Second Commit for the Feature Branch
    
    * Added Third Commit for the Feature branch

--- main.go
+++ main.go
@@ -2,4 +2,10 @@ package main
 
 func main() {
 	print("This is the Main Branch")
+
+	print("First Commit for the Feature Branch")
+
+	print("Second Commit for the Feature  Branch")
+
+	print("Third Commit for the Feature Branch after which We will merge into the main branch")
 }
